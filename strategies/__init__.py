a2a94419d56d98b1cb536f7b3eef911ddc46a871
yasserrida <ridayassir.yr@gmail.com>
2021-06-10 14:41:33 +0100

    add files from local

---
+++ strategies/__init__.py
@@ -0,0 +1,5 @@
+from .random_sampling import RandomSampling
+from .least_confidence import LeastConfidence
+from .margin_sampling import MarginSampling
+from .entropy_sampling import EntropySampling
+from .kmeans_sampling import KMeansSampling
