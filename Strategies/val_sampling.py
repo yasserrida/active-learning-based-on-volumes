e734bf394dfda8dcd81a6ce8215111054578114d
yasserrida <ridayassir.yr@gmail.com>
2021-06-14 18:55:19 +0100

    update val algo

--- Strategies/val_sampling.py
+++ Strategies/val_sampling.py
@@ -1,58 +1,18 @@
 import numpy as np
 import torch
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import DataLoader
+from Helpers.strategie import Strategie
 
 
-class VAL:
-    def __init__(self, x_train, y_train, idxs_labeled, classifier, handler, args):
-        self.x_train = x_train
-        self.y_train = y_train
-        self.idxs_labeled = idxs_labeled
-        self.classifier = classifier
-        self.handler = handler
-        self.args = args
-        self.n_pool = len(y_train)
-        use_cuda = torch.cuda.is_available()
-        self.device = torch.device("cuda" if use_cuda else "cpu")
+class VAL(Strategie):
+    def __init__(self, x_train, y_train, idxs_labeled, net, handler, args):
+        super(VAL, self).__init__(
+            x_train, y_train, idxs_labeled, net, handler, args)
 
+    # ======== Selectionner les point avec le score de confiance le plus elevé
     def query(self, n):
-        pass
-
-    def update(self, idxs_labeled):
-        self.idxs_labeled = idxs_labeled
-
-    def _train(self, epoch, loader_tr, optimizer):
-        self.clf.train()
-        for batch_idx, (x, y, idxs) in enumerate(loader_tr):
-            x, y = x.to(self.device), y.to(self.device)
-            optimizer.zero_grad()
-            out, e1 = self.clf(x)
-            loss = F.cross_entropy(out, y)
-            loss.backward()
-            optimizer.step()
-
-    def train(self):
-        n_epoch = self.args['n_epoch']
-        self.clf = self.classifier().to(self.device)
-        optimizer = optim.SGD(self.clf.parameters(), **
-                              self.args['optimizer_args'])
-        idxs_train = np.arange(self.n_pool)[self.idxs_labeled]
-        loader_tr = DataLoader(self.handler(self.x_train[idxs_train], self.y_train[idxs_train],
-                               transform=self.args['transform']), shuffle=True, **self.args['loader_tr_args'])
-        for epoch in range(1, n_epoch+1):
-            self._train(epoch, loader_tr, optimizer)
-
-    def predict(self, x_train, y_train):
-        loader_te = DataLoader(self.handler(
-            x_train, y_train, transform=self.args['transform']), shuffle=False, **self.args['loader_te_args'])
-        self.clf.eval()
-        P = torch.zeros(len(y_train), dtype=y_train.dtype)
-        with torch.no_grad():
-            for x, y, idxs in loader_te:
-                x, y = x.to(self.device), y.to(self.device)
-                out, e1 = self.clf(x)
-                pred = out.max(1)[1]
-                P[idxs] = pred.cpu()
-        return P
+        idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_labeled]
+        probs = self.predict_prob(
+            self.x_train[idxs_unlabeled], self.y_train[idxs_unlabeled])
+        log_probs = torch.log(probs)
+        U = (probs*log_probs).sum(1)
+        return idxs_unlabeled[U.sort()[1][:n]]
